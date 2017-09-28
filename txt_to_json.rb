d5307f8f41663085bd878f18be7f1380e2d336c0
Daniel Maher <phrawzty+github@gmail.com>
2017-09-28 14:14:00 +0200

    add some reporting

--- txt_to_json.rb
+++ txt_to_json.rb
@@ -11,8 +11,8 @@ OptionParser.new do |opts|
     opts.on('--sourcedir DIR', 'Directory where .txt files are located.') do |v|
         options[:sourcedir] = v
     end
-    opts.on('--outputdir DIR', 'Direcotry where .json will end up (same as sourcedir by default).') do |v|
-        options[:outputdir] = v
+    opts.on('--destdir DIR', 'Direcotry where .json will end up (same as sourcedir by default).') do |v|
+        options[:destdir] = v
     end
 
     opts.on('-h', '--help', 'This help.') do
@@ -28,8 +28,8 @@ if not (options[:sourcedir]) then
 end
 
 # Set output dir to source dir unless otherwise specified.
-if not (options[:outputdir]) then
-    options[:outputdir] = options[:sourcedir]
+if not (options[:destdir]) then
+    options[:destdir] = options[:sourcedir]
 end
 
 # Ensure dirs are r/w as necessary.
@@ -40,12 +40,14 @@ else
     exit 1
 end
 
-if File.writable?(options[:outputdir]) then
-    puts 'Writing to ' + options[:outputdir]
+if File.writable?(options[:destdir]) then
+    puts 'Writing to ' + options[:destdir]
 else
-    puts 'Cannot write to ' + options[:outputdir]
+    puts 'Cannot write to ' + options[:destdir]
     exit 1
 end
+
+i = 0
 
 Dir.glob(options[:sourcedir] + '/*.txt') do |txtfile|
     # Parse the contents; in this case, zero or more email addresses.
@@ -67,7 +69,15 @@ Dir.glob(options[:sourcedir] + '/*.txt') do |txtfile|
     obj['categories'] = {}
 
     # Make JSON from object
-    File.open(options[:outputdir] + '/' + name + '.json', 'w') do |f|
+    File.open(options[:destdir] + '/' + name + '.json', 'w') do |f|
         f.puts JSON.pretty_generate(obj)
     end
+
+    puts 'Processed ' + txtfile
+
+    i += 1
 end
+
+puts 'Total processed files: ' + i.to_s
+
+exit 0
